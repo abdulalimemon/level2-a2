22265b6c1e768246dca8d42e793aaac412bdca26
emon02 <abdulalimemon11@gmail.com>
2024-07-05 02:05:22 +0600

    added controller route and service file

--- src/app.ts
+++ src/app.ts
@@ -1,11 +1,15 @@
 import express, { Application, Request, Response } from 'express';
 import cors from 'cors';
+import { productRoutes } from './modules/product/product.route';
 
 const app: Application = express();
 
 // parser
 app.use(express.json());
 app.use(cors());
+
+// routes
+app.use('/api/products', productRoutes);
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Hello World.');
--- src/modules/product/product.model.ts
+++ src/modules/product/product.model.ts
@@ -1,6 +1,65 @@
 import { Schema, model } from 'mongoose';
-import { TProduct } from './product.interface';
+import { TInventory, TProduct, TVariants } from './product.interface';
 
-const productSchema = new Schema<TProduct>({});
+const variantsSchema = new Schema<TVariants>({
+  type: {
+    type: String,
+    required: [true, 'Product type is required.'],
+    trim: true,
+  },
+  value: {
+    type: String,
+    required: [true, 'Product value is required.'],
+    trim: true,
+  },
+});
+
+const inventorySchema = new Schema<TInventory>({
+  quantity: {
+    type: Number,
+    required: [true, 'Product quantity is required.'],
+    trim: true,
+  },
+  inStock: {
+    type: Boolean,
+  },
+});
+
+const productSchema = new Schema<TProduct>({
+  name: {
+    type: String,
+    required: [true, 'Product name is required.'],
+    trim: true,
+    unique: true,
+  },
+  description: {
+    type: String,
+    required: [true, 'Product description is required.'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price is required.'],
+    trim: true,
+  },
+  category: {
+    type: String,
+    required: [true, 'Product category is required.'],
+    trim: true,
+  },
+  tags: {
+    type: [String],
+    required: [true, 'Product tags is required.'],
+    trim: true,
+  },
+  variants: {
+    type: [variantsSchema],
+    required: [true, 'Product variants is required.'],
+  },
+  inventory: {
+    type: inventorySchema,
+    required: [true, 'Product inventory is required.'],
+  },
+});
 
 export const ProductModel = model<TProduct>('Product', productSchema);
