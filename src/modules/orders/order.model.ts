fca82bd25808135661258ceba10a1eca04aecf04
emon02 <abdulalimemon11@gmail.com>
2024-07-05 16:36:55 +0600

    create interface and model for orders

---
+++ src/modules/orders/order.model.ts
@@ -0,0 +1,23 @@
+import { TOrder } from './order.interface';
+import { model, Schema } from 'mongoose';
+
+const orderSchema = new Schema<TOrder>({
+  email: {
+    type: String,
+    required: [true, 'Email is required.'],
+    trim: true,
+    unique: true,
+  },
+  productId: {
+    type: String,
+    required: [true, 'Product id is required.'],
+    trim: true,
+  },
+  price: {
+    type: Number,
+    required: [true, 'Product price is required.'],
+    trim: true,
+  },
+});
+
+export const OrderModel = model<TOrder>('Order', orderSchema);
