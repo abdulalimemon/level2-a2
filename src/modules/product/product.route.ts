eabb06ee57ce40dc8becedd9f647bff59d7fe565
emon02 <abdulalimemon11@gmail.com>
2024-07-05 16:06:00 +0600

    Retrieve a List of All Products

--- src/modules/product/product.route.ts
+++ src/modules/product/product.route.ts
@@ -4,5 +4,6 @@ import { ProductControllers } from './product.controller';
 const router = express.Router();
 
 router.post('/create-product', ProductControllers.createProduct);
+router.get('/', ProductControllers.getAllProducts);
 
 export const productRoutes = router;
